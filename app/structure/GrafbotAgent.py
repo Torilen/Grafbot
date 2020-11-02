196dafec2ee4d5f90bb37ef40135c16965648ccc
Aniss Bentebib <abentebib@asmodine.com>
2020-11-02 18:32:30 +0100

    remove translation

--- app/structure/GrafbotAgent.py
+++ app/structure/GrafbotAgent.py
@@ -40,9 +40,10 @@ class GrafbotAgent:
 
     def speak(self, reply_text):
         print("Reply : "+reply_text)
-        user_language = detect(reply_text)
+        #user_language = detect(reply_text)
 
-        english_version_of_user_input = translate_base(reply_text, src=user_language)
+        #english_version_of_user_input = translate_base(reply_text, src=user_language)
+        english_version_of_user_input = reply_text
         entities = get_entities(english_version_of_user_input)
         stories = self.semkg.get_stories(self.epikg, [x[0] for x in entities])
         print("STORIES: ")
